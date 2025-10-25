aede8561093323e5a709dd7e47b45656df75e22e
islam0122 <duishobaevislam01@gmail.com>
2025-10-26 01:52:08 +0600

    day_08

---
+++ day_08.py
@@ -0,0 +1,60 @@
+"""
+ Partial Word Searching --> 7kyu
+"""
+# def word_search(query, seq):
+#     result = []
+#     for word in seq:
+#         if query.lower() in word.lower():
+#             result.append(word)
+#     return result or ["None"]
+#
+# print(word_search("ab", ["za", "ab", "abc", "zab", "zbc"]))
+
+"""
+Function 2 - squaring an argument --> 8kyu
+"""
+# def square(n):
+#     return n ** 2
+# print(square(5))
+
+"""
+Grasshopper - Messi goals function --> kyu
+"""
+# def goals(laLiga, copaDelRey, championsLeague):
+#     return laLiga + copaDelRey + championsLeague
+
+""" 
+Sum of odd numbers --> 7kyu
+"""
+# def row_sum_odd_numbers(n):
+#     num = 1
+#     result = []
+#     for i in range(1, n + 1):
+#         row = []
+#         for j in range(i):
+#             row.append(num)
+#             num += 2
+#         result.append(row)
+#     return sum(result[n-1])
+#
+# print(row_sum_odd_numbers(2))
+
+"""
+Duplicate Arguments --> 6kyu
+ """
+# def solution(*args):
+#     return len(args) != len(set(args))
+#
+# print(solution(1, 2, 3,))
+
+"""
+Sum of array singles --> 7kyu
+"""
+# def repeats(arr):
+#     unique = []
+#     for i in arr:
+#         if arr.count(i) == 1:
+#             unique.append(i)
+#     return sum(unique)
+#
+# print(repeats([4,5,7,5,4,8]))
